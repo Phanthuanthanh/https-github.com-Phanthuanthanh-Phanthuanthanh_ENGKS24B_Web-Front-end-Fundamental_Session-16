c6247acbb91742bc7c81dd4bef6da6d6d595d75d
Phanthuanthanh/Phanthuanthanh_ENGKS24B_Project-Flappy-bird <Phanthuanthanh270606@gmail.com>
2025-02-26 17:07:34 +0700

    Up load Bài tập Session 16_Lesson 05

---
+++ Session 16_Lesson 05.js
@@ -0,0 +1,7 @@
+let firstNumber = prompt("Mời bạn nhập số thứ nhất:");
+
+let secondNumber = prompt("Mời bạn nhập số thứ hai:");
+
+let result= firstNumber + secondNumber;
+alert(`Tổng số thứ nhất và số thứ hai là: ${result}`);
+
