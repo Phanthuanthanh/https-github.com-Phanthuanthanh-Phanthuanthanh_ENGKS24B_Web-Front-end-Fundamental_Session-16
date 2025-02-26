996bc17e4321fc6276d987b91e6acd0ef9541d69
Phanthuanthanh/Phanthuanthanh_ENGKS24B_Project-Flappy-bird <Phanthuanthanh270606@gmail.com>
2025-02-26 17:07:17 +0700

    Up load Bài tập Session 16_Lesson 07

---
+++ Session 16_Lesson 07.js
@@ -0,0 +1,11 @@
+let math = 9;
+
+let physics = 6;
+
+let chemistry = 8;
+
+let total = math + physics + chemistry;
+
+let medium = total/3;
+
+alert(`Điểm trung bình của 3 môn trên là là: ${medium}đ`);
